64220b782a1e2212745e8625115e306c48079981
Archangel75 <garrrus@yandex.ru>
2018-10-14 13:44:15 +0300

    Started the task

---
+++ TestForVacancyProject/RoandArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForVacancyProject
+{
+    public class RoundArea : BaseAreaCounter, IAreaCounter
+    {
+        ///<summary>
+        ///<para> Возвращает площадь круга.</para>
+        ///</summary>
+        public double Area() => Math.Round(Math.Pow(Convert.ToDouble(radius), 2) * 3.1415, 2);
+    }
+}
---
+++ TestForVacancyProject/TriangleArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForVacancyProject
+{
+    public class TriangleArea : BaseAreaCounter, IAreaCounter
+    {
+        private double Perimeter()
+        {
+            return aSide + bSide + cSide;
+        }
+
+        ///<summary>
+        ///<para> Возвращает площадь треугольника </para>
+        ///</summary>
+        public double Area()
+        {
+            double p = Perimeter();
+            return Math.Round(Math.Sqrt(p * (p - aSide) * (p - bSide) * (p - cSide)), 2);
+        }
+
+    }
+}
